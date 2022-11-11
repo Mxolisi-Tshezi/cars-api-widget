d313ce2e5b127f634e93f8233ffcc7db06491ec4
Mxolisi <nyawose57@gmail.com>
2022-11-11 07:32:15 +0200

    save changes

---
+++ public/cars.js
@@ -0,0 +1,40 @@
+const colorsTemplateSource = document.querySelector('.colorsTemplate');
+const colorsTemplate = Handlebars.compile(colorsTemplateSource.innerHTML);
+
+const brandsTemplateSource = document.querySelector('.brandsTemplate');
+const brandsTemplate = Handlebars.compile(brandsTemplateSource.innerHTML);
+
+const allCarsTemplateSource = document.querySelector('.allCarsTemplate');
+const allCarsTemplate = Handlebars.compile(allCarsTemplateSource.innerHTML);
+
+
+axios.get('http://api-tutor.herokuapp.com/v1/cars')
+.then(function (response) {
+
+  const allCarsElem = document.querySelector('.cars');
+  allCarsElem.innerHTML = allCarsTemplate({
+     allCars: response.data
+  })
+  console.log(response.data)
+});
+
+axios.get('http://api-tutor.herokuapp.com/v1/colors')
+  .then(function (response) {
+
+    const colorsElem = document.querySelector('.colors');
+    colorsElem.innerHTML = colorsTemplate({
+        colors: response.data
+    })
+  });
+
+
+  axios.get('http://api-tutor.herokuapp.com/v1/makes')
+  .then(function (response) {
+
+     const brandsElem = document.querySelector('.brands');
+     brandsElem.innerHTML = brandsTemplate({
+         brands: response.data
+     })
+  });
+
+  
